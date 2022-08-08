998b1bd3315d9c31e4e9eabdd81451e65b10a2ca
Adriano Romero <adriano.romerofco@gmail.com>
2022-08-08 14:13:33 -0300

    setup dos components e imagens.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { FilterComponent } from './components/bookstore-app/filter/filter.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookstoreAppComponent,
+    FilterComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
